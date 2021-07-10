3e706781f9108cf2a6dc320eca7643a6c26bc969
toastedguy2 <toastedguy2@protonmail.com>
2021-07-09 19:37:38 -0600

    Mark-up done

--- src/App.js
+++ src/App.js
@@ -1,118 +1,165 @@
-import React from "react";
-
+import React, { useState, useRef, useEffect } from "react";
 export default function App() {
-  // TODO: Display buttons on column
+  const [toggle, setToggle] = useState(false);
+  const modal = useRef(null);
+  useEffect(() => {
+    window.addEventListener("click", (e) => {
+      if (e.target === modal.current) {
+        setToggle(false);
+      }
+    });
+  }, []);
   return (
-    // <div className="game-menu-screen">
-    //   <h1 className="menu-screen-title">Balloon Popping</h1>
-    //   {/* <div className="btn-container">
-    //     <button type="button" className="btn primary-btn start-btn">
-    //       START
-    //     </button>
-    //     <button type="button" className="btn secondary-btn rules-btn">
-    //       Rules
-    //     </button>
-    //     <button type="button" className="btn white-btn records-btn">
-    //       Records
-    //     </button>
-    //   </div>
-    // </div> */}
-    <div>
-      <nav className="navbar">
-        <div className="balloon-to-pop-toolbar">
-          <h1 className="balloon-to-pop-title">Balloon to POP </h1>
-          <div className="balloon-to-pop"></div>
-        </div>
-        <div className="switching-balloon-in-toolbar">
-          <h1 className="switching-balloon-in-title">Switching balloon in</h1>
-          <h1 className="switching-balloon-in-time">0:00</h1>
-        </div>
-        <h1 className="settings">
-          <i className="fas fa-cogs fa-3x"></i>
-        </h1>
-      </nav>
-      <div className="balloons">
-        <div className="balloons-row">
-          <div className="balloon-popped">POP</div>
-        </div>
-        <div className="balloons-row">
-          <div className="balloon"></div>
-          <div className="balloon green-balloon"></div>
-          <div className="balloon white-balloon"></div>
-          <div className="balloon red-balloon"></div>
-          <div className="balloon yellow-balloon"></div>
-        </div>
-        <div className="balloons-row">
-          <div className="balloon"></div>
-          <div className="balloon green-balloon"></div>
-          <div className="balloon white-balloon"></div>
-          <div className="balloon red-balloon"></div>
-          <div className="balloon yellow-balloon"></div>
-        </div>
-        <div className="balloons-row">
-          <div className="balloon"></div>
-          <div className="balloon green-balloon"></div>
-          <div className="balloon white-balloon"></div>
-          <div className="balloon red-balloon"></div>
-          <div className="balloon yellow-balloon"></div>
-        </div>
-        <div className="balloons-row">
-          <div className="balloon"></div>
-          <div className="balloon green-balloon"></div>
-          <div className="balloon white-balloon"></div>
-          <div className="balloon red-balloon"></div>
-          <div className="balloon yellow-balloon"></div>
+    <>
+      <div className="game-menu-screen">
+        <div className="game-menu-screen-content">
+          <h1 className="menu-screen-title">Balloon Popping</h1>
+          <div className="btn-wrapper">
+            <button type="button" className="btn primary-btn start-btn">
+              <i class="fas fa-gamepad"></i> START
+            </button>
+            <button
+              type="button"
+              className="btn secondary-btn rules-btn"
+              onClick={() => setToggle(true)}
+            >
+              <i class="fas fa-question"></i> Rules
+            </button>
+            <button type="button" className="btn white-btn records-btn">
+              <i class="fas fa-star"></i> Records
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-
-    // GAME STATS SCREEN
-
-    // <div className="game-stats-screen">
-    //   <header className="game-stats-header">
-    //     <h1 className="game-stats-title">Game Stats</h1>
-    //     <h3 className="game-stats-ended-in-title">Ended in</h3>
-    //     <h1 className="time">12:00</h1>
-    //     <h1 className="new-record-title">New Record</h1>
-    //   </header>
+      <div className={`modal ${toggle && "show-modal"}`} ref={modal}>
+        <div className="modal__content">
+          <div className="modal__content__header">
+            <h1 className="modal__content__header__title">Rules</h1>
+            <button
+              className="modal__content__header__close-btn"
+              onClick={() => setToggle(false)}
+            >
+              X
+            </button>
+          </div>
+          <div className="modal__body">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+              molestias culpa explicabo tenetur quaerat labore impedit eaque
+              officiis sapiente aliquid.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+    // <>
     //   <div>
-    //     <h1 className="balloons-popped-title">Balloons Popped</h1>
-    //     <div className="balloon-stats">
-    //       <div className="balloon-stat">
-    //         <div className="balloon red-balloon"></div>
-    //         <h1 className="balloon-stat-quantity">23</h1>
-    //       </div>
-    //       <div className="balloon-stat">
-    //         <div className="balloon green-balloon"></div>
-    //         <h1 className="balloon-stat-quantity">10</h1>
-    //       </div>
-    //       <div className="balloon-stat">
-    //         <div className="balloon yellow-balloon"></div>
-    //         <h1 className="balloon-stat-quantity">3</h1>
-    //       </div>
-    //       <div className="balloon-stat">
-    //         <div className="balloon white-balloon"></div>
-    //         <h1 className="balloon-stat-quantity">9</h1>
-    //       </div>
-    //       <div className="balloon-stat">
-    //         <div className="balloon black-balloon"></div>
-    //         <h1 className="balloon-stat-quantity">5</h1>
-    //       </div>
-    //     </div>
-    //     <div className="game-stats-summary">
-    //       <p>
-    //         <span className="total-popped">Total popped:</span> 23
-    //       </p>
-    //       <p>
-    //         <span className="lost-balloons">Lost:</span> 5{" "}
-    //       </p>
+    //     <nav className="toolbar">
+    //       <div className="balloon-to-pop"></div>
+    //       <h1 className="time-left">0:00</h1>
+    //       <button className="settings" onClick={() => setToggle(true)}>
+    //         <i className="fas fa-cogs fa-3x"></i>
+    //       </button>
+    //     </nav>
+    //     <div className="balloons">
+    //       <div className="balloon"></div>
+    //       <div className="balloon green-balloon"></div>
+    //       <div className="balloon white-balloon"></div>
+    //       <div className="balloon red-balloon"></div>
+    //       <div className="balloon yellow-balloon"></div>
+    //       <div className="balloon"></div>
+    //       <div className="balloon green-balloon"></div>
+    //       <div className="balloon white-balloon"></div>
+    //       <div className="balloon red-balloon"></div>
+    //       <div className="balloon yellow-balloon"></div>
+    //       <div className="balloon"></div>
+    //       <div className="balloon green-balloon"></div>
+    //       <div className="balloon white-balloon"></div>
+    //       <div className="balloon red-balloon"></div>
+    //       <div className="balloon yellow-balloon"></div>
+    //       <div className="balloon"></div>
+    //       <div className="balloon green-balloon"></div>
+    //       <div className="balloon white-balloon"></div>
+    //       <div className="balloon red-balloon"></div>
+    //       <div className="balloon yellow-balloon"></div>
     //     </div>
     //   </div>
-    //   <div className="btn-container">
-    //     <button className="btn primary-btn">RESTART</button>
-    //     <button className="outline-btn primary-outline-btn">
-    //       Back to menu
-    //     </button>
+    //   <div className={`modal ${toggle && "show-modal"}`} ref={modal}>
+    //     <div className="modal__content">
+    //       <div className="modal__content__header">
+    //         <h1 className="modal__content__header__title">Settings</h1>
+    //         <button
+    //           className="modal__content__header__close-btn"
+    //           onClick={() => setToggle(false)}
+    //         >
+    //           X
+    //         </button>
+    //       </div>
+    //       <div
+    //         className="modal__body"
+    //         style={{ display: "flex", flexDirection: "column" }}
+    //       >
+    //         <button type="button" className="btn primary-btn">
+    //           <i class="fas fa-sync-alt"></i> Restart
+    //         </button>
+    //         <button type="button" className="outline-btn secondary-outline-btn">
+    //           <i class="fas fa-skull-crossbones"></i> Quit
+    //         </button>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </>
+    // GAME STATS SCREEN
+
+    // <div className="container">
+    //   <div className="game-stats">
+    //     <header className="game-stats-header">
+    //       <h1 className="game-stats-title">Game Stats</h1>
+    //       <h3 className="game-stats-ended-in-title">Ended in</h3>
+    //       <h1 className="time">12:00</h1>
+    //       <h1 className="new-record-title">New Record</h1>
+    //     </header>
+    //     <div>
+    //       <h1 className="balloons-popped-title">Balloons Popped</h1>
+    //       <div className="balloon-stats">
+    //         <div className="balloon-stat">
+    //           <div className="balloon red-balloon"></div>
+    //           <h1 className="balloon-stat-quantity">23</h1>
+    //         </div>
+    //         <div className="balloon-stat">
+    //           <div className="balloon green-balloon"></div>
+    //           <h1 className="balloon-stat-quantity">10</h1>
+    //         </div>
+    //         <div className="balloon-stat">
+    //           <div className="balloon yellow-balloon"></div>
+    //           <h1 className="balloon-stat-quantity">3</h1>
+    //         </div>
+    //         <div className="balloon-stat">
+    //           <div className="balloon white-balloon"></div>
+    //           <h1 className="balloon-stat-quantity">9</h1>
+    //         </div>
+    //         <div className="balloon-stat">
+    //           <div className="balloon black-balloon"></div>
+    //           <h1 className="balloon-stat-quantity">5</h1>
+    //         </div>
+    //       </div>
+    //       <div className="game-stats-summary">
+    //         <p>
+    //           <span className="total-popped">Total popped:</span> 23
+    //         </p>
+    //         <p>
+    //           <span className="lost-balloons">Lost:</span> 5{" "}
+    //         </p>
+    //       </div>
+    //     </div>
+    //     <div className="btn-container">
+    //       <button className="btn primary-btn">
+    //         <i class="fas fa-sync-alt"></i> RESTART
+    //       </button>
+    //       <button className="outline-btn secondary-outline-btn">
+    //         <i class="fas fa-arrow-left"></i> Back to menu
+    //       </button>
+    //     </div>
     //   </div>
     // </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./reset.css";
 import "./common.css";
+import "./modal.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import App from "./App";
 
